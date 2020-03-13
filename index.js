a2d829983c1ec16118f531d4b7e8b95e96d49046
ildarmgt <ildarmgt@gmail.com>
2020-03-13 06:22:06 -0400

    nice colors

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// @ts-nocheck
-
 /* -------------------------------------------------------------------------- */
 /*                                  settings                                  */
 /* -------------------------------------------------------------------------- */
 
 const settings = {
-  unit: '4.3vmax', // unit size
+  unit: '3.81vmin', // unit size
   size: 21, // area side in units
   figs: 2, // sig figs for units
-  nucleusColor: [0, 0, 40, 0.5], // cell center
-  wobbleColor: [0, 0, 80, 0.5], // goo
-  wobbleColorIfNoNucleus: [0, 0, 80, 0.7],
-  deadColor: [200, 200, 200, 0.06], // background
-  deadColorIfNoNucleus: [200, 200, 200, 0.16],
+  nucleusColor: [0, 0, 40, 0.4], // cell center
+  wobbleCellColor: [0, 0, 80, 0.6], // goo
+  wobbleCellColorIfNoNucleus: [0, 0, 80, 0.7],
+  backgroundColor: [200, 200, 200, 0.3], // background
+  backgroundColorIfNoNucleus: [200, 200, 200, 0.4], // 0.16
   // prettier-ignore
   seed: (
     '000000000000000000000' +
@@ -25,16 +23,16 @@ const settings = {
     '011110010010000011110' +
     '000000000000000000000'
   ).repeat(3),
-  shiftX: 3, // hides divpen div
-  shiftY: 3, // hides divpen div
+  shiftX: 10, // hides divpen div
+  shiftY: 10, // hides divpen div
   rndScale: 0.9, // fraction of wobble
   blurScale: 1 / 6,
-  framesPerSecond: 9, // more here is big impact on cpu
-  secondsPerStep: 5,
+  framesPerSecond: 12, // more here is big impact on cpu
+  secondsPerStep: 3,
   bubbles: true,
   renderNucleus: false,
   wobbleTransition: '0.4s', // ~ wobble smoothness
-  stepTransition: '0.15s', // ~ step change render
+  stepTransition: '0.2s', // ~ step change render
   /* -------------------------------------------------------------------------- */
   /*                                  settings end                              */
   /* -------------------------------------------------------------------------- */
@@ -50,12 +48,10 @@ const mouse = {
 }
 
 const main = () => {
-  console.log('\n\n it starts... \n\n')
-
   initializePenCss()
 
-  // start rendering with first frame no transitions
-  drawFrame(thisLife(settings.seed).updateCss(true))
+  // start rendering loop (no transition on 1st frame)
+  drawFrame(thisLife(settings.seed).updateCss(1))
 
   window.addEventListener('mousemove', updateMouseXY)
   window.addEventListener('mousedown', updateMouseXY)
@@ -130,11 +126,11 @@ const color = (a, b = a, amt = 0.0) => {
 }
 
 /* -------------------------------------------------------------------------- */
-/*                           pen css from life array                          */
+/*                               calc cells css                               */
 /* -------------------------------------------------------------------------- */
 
 // set changing css for pen
-const setPenCss = (life, simple = false) => {
+const setPenCss = (life, transitionType = 0) => {
   let boxShadow = ''
 
   // alive on top
@@ -146,46 +142,59 @@ const setPenCss = (life, simple = false) => {
     const vx = cx + settings.shiftX
     const vy = cy + settings.shiftY
     const nucleusColor = color(settings.nucleusColor).toString()
-    const deadColor = color(
+    const deadColor = color([0, 0, 0, 0]).toString()
+    let wobbleCellColor = color(
       settings.renderNucleus
-        ? settings.deadColor
-        : settings.deadColorIfNoNucleus
+        ? settings.wobbleCellColor
+        : settings.wobbleCellColorIfNoNucleus
     ).toString()
-    const wobbleColor = color(
-      settings.renderNucleus
-        ? settings.wobbleColor
-        : settings.wobbleColorIfNoNucleus
-    ).toString()
-    const wobbleSize = len(0 + Math.random() * settings.rndScale)
+    // if (Math.random() < 0.05) wobbleCellColor = color().toString()
+    const wobbleSize = len(Math.random() * settings.rndScale)
     const xWobble = len(vx + (Math.random() - 0.5) * settings.rndScale)
     const yWobble = len(vy + (Math.random() - 0.5) * settings.rndScale)
+    // prevents patterns on dense pops
+    const deadRadius = len(Math.random() - 0.5)
+
     // living cells
     if (life[i].alive) {
       boxShadow += settings.renderNucleus
-        ? `\t${len(vx)} ${len(vy)} 0 ${len(0)} ${nucleusColor},\n`
+        ? // small nucleus
+          `\t${xWobble} ${yWobble} 0 ${len(-0.1)} ${nucleusColor},\n`
         : ''
-      boxShadow += `\t${xWobble} ${yWobble} 0 ${wobbleSize} ${wobbleColor},\n`
+      boxShadow += `\t${xWobble} ${yWobble} 0 ${wobbleSize} ${wobbleCellColor},\n`
     }
 
-    // dead cells for contrast, keep same # of items
+    // keep same # of items same
     // to prevent order reorganizations and smooth transitions
     if (!life[i].alive) {
       boxShadow += settings.renderNucleus
-        ? `\t${len(vx)} ${len(vy)} 0 ${len(0.5)} ${deadColor},\n`
+        ? `\t${len(vx)} ${len(vy)} 0 ${len(-1)} ${deadColor},\n`
         : ''
 
-      boxShadow += `\t${len(vx)} ${len(vy)} 0 ${len(0.5)} ${deadColor},\n`
+      boxShadow += `\t${len(vx)} ${len(vy)} 0 ${deadRadius} ${deadColor},\n`
     }
   }
 
+  boxShadow += `
+    ${len(settings.size / 2 + settings.shiftX)}
+    ${len(settings.size / 2 + settings.shiftY)}
+    0
+    ${len(settings.size * 2)}
+    ${color(
+      settings.renderNucleus
+        ? settings.backgroundColor
+        : settings.backgroundColorIfNoNucleus
+    ).toString()}
+  `
+
   let transition = ''
   // wobble transition
-  if (simple == 0)
+  if (transitionType == 0)
     transition = `transition: box-shadow ${settings.wobbleTransition};\n`
-  // no transition on loading the page
-  if (simple == 1) transition = 'transition: none;\n'
-  // on step change fade out is ugly so speed it up
-  if (simple == 2)
+  // no transition (on loading the page)
+  if (transitionType == 1) transition = 'transition: none;\n'
+  // on step change death/birth
+  if (transitionType == 2)
     transition = `transition: box-shadow ${settings.stepTransition};\n`
 
   boxShadow = '\nbox-shadow:\n' + boxShadow.slice(0, -2) + ';\n' + transition
@@ -193,33 +202,25 @@ const setPenCss = (life, simple = false) => {
   updateHeader(`#divpen {` + boxShadow + `}`)
 }
 
-// set constant css for pen
+// set static css based on settings
 const initializePenCss = () => {
   // 3d translate caused edge blur here, not sure why, avoid.
   const basics =
     ` #divpen {
-    will-change: box-shadow, filter;
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
     width: ${len(1)};
     height: ${len(1)};
-    background-color: rgba(255, 255, 255, 0);
-    pointer-events: none;
-    border-radius: 100%;
     transform: translate(${len(-settings.shiftX)}, ${len(-settings.shiftY)});
   ` +
     (settings.bubbles
       ? `filter: blur(
         ${len(settings.blurScale)}
-        ) saturate(5) contrast(40) saturate(0.16);`
+        ) saturate(5) contrast(60) saturate(0.16);`
       : '') +
     '}'
-
   updateHeader(basics, 'penbasics')
 }
 
+// puts new style tag into header with css
 const updateHeader = (cssString, forWho = 'divpen') => {
   // remove old style
   if (window.for_divpen) {
@@ -234,17 +235,29 @@ const updateHeader = (cssString, forWho = 'divpen') => {
   document.head.appendChild(styleEl)
 }
 
+/* -------------------------------------------------------------------------- */
+/*                        game of life state functions                        */
+/* -------------------------------------------------------------------------- */
+
 const thisLife = (life = null) => {
-  // git init
+  // deep copy life (takes too long)
+  // if (life) life = JSON.parse(JSON.stringify(life))
+  if (Array.isArray(life)) life = life.map(v => v)
+
+  // init
   if (!life) {
+    console.log('\n'.repeat(10), 'it starts... \n\n')
     const newLife = new Array(settings.size * settings.size).fill({
       alive: false,
       count: 0
     })
     return thisLife(newLife)
+      .updateAllCounts()
+      .printLog(`step ${settings.step++}`)
   }
-
+  // init with seed
   if (typeof life === 'string') {
+    console.log('\n'.repeat(10), 'it starts... \n\n')
     let lifeSeed = life
       .repeat(10)
       .split('')
@@ -259,9 +272,11 @@ const thisLife = (life = null) => {
         alive: lifeSeed[i] ? lifeSeed[i] === 1 : v.alive
       }))
     )
+      .updateAllCounts()
+      .printLog(`step ${settings.step++}`)
   }
 
-  // reset counts to 0
+  // reset counts for all to 0
   const resetCounts = () => {
     life.forEach((_, i) => (life[i].count = 0))
     return thisLife(life)
@@ -278,10 +293,10 @@ const thisLife = (life = null) => {
     return thisLife(life)
   }
 
-  // hey y'all
+  // xy cell updates nearby frands
   const updateNearbyCounts = (x, y) => {
     if (isAlive(x, y)) {
-      ;[
+      const posToUpdate = [
         [-1, -1],
         [0, -1],
         [1, -1],
@@ -290,18 +305,19 @@ const thisLife = (life = null) => {
         [-1, 1],
         [0, 1],
         [1, 1]
-      ].forEach(delta => addToCount(x + delta[0], y + delta[1]))
+      ]
+      posToUpdate.forEach(delta => addToCount(x + delta[0], y + delta[1]))
     }
     return thisLife(life)
   }
 
-  // count frands
+  // get frands count at xy
   const getCount = (x, y) => {
     const index = y * settings.size + x
     return life[index].count
   }
 
-  // add to surrounding cells from 1 or 0
+  // update counts of all cells
   const updateAllCounts = () => {
     resetCounts()
     for (let i = 0; i < life.length; i++) {
@@ -312,24 +328,28 @@ const thisLife = (life = null) => {
     return thisLife(life)
   }
 
-  // u there?
+  // check hp
   const isAlive = (x, y) => {
     // if outside range label as bad
-    if (y >= settings.size || x >= settings.size || y < 0 || x < 0) return null
+    if (y >= settings.size || x >= settings.size || y < 0 || x < 0) return false
     // otherwise return state at index
     const index = y * settings.size + x
     return life[index].alive
   }
 
+  // rez
   const setAlive = (x, y) => {
-    if (y >= settings.size || x >= settings.size || y < 0 || x < 0)
-      return thisLife(life)
+    const inRange =
+      x !== undefined &&
+      y !== undefined &&
+      !(y >= settings.size || x >= settings.size || y < 0 || x < 0)
+    if (!inRange) return thisLife(life)
     const index = y * settings.size + x
     life[index].alive = true
     return thisLife(life)
   }
 
-  // use counts of each cell to change its living
+  // set all lives from counts
   const updateAllLives = () => {
     const newLife = []
     for (let i = 0; i < life.length; i++) {
@@ -341,7 +361,7 @@ const thisLife = (life = null) => {
     return thisLife(newLife)
   }
 
-  // cells doing cell things
+  // check if cell alive next step
   const isAliveNextStep = (x, y) => {
     const thisCount = getCount(x, y)
     const thisAlive = isAlive(x, y)
@@ -356,8 +376,8 @@ const thisLife = (life = null) => {
   const takeStep = () => {
     return thisLife(life)
       .updateAllCounts()
-      .printLog(`step ${settings.step++}`)
       .updateAllLives()
+      .printLog(`step ${settings.step++}`)
   }
 
   const printLog = note => {
@@ -374,8 +394,8 @@ const thisLife = (life = null) => {
     return thisLife(life)
   }
 
-  const updateCss = (simple = false) => {
-    setPenCss(life, simple)
+  const updateCss = (transitionType = 0) => {
+    setPenCss(life, transitionType)
     return thisLife(life)
   }
 
@@ -409,7 +429,7 @@ const thisLife = (life = null) => {
 const getUnitInPx = unitToMeasureInPx => {
   const div = document.createElement('div')
   div.style.width = 'calc(1000 * ' + unitToMeasureInPx + ')'
-  window.wrapper.appendChild(div)
+  if (window.wrapper) window.wrapper.appendChild(div)
   const units1000 = +window.getComputedStyle(div).width.replace('px', '')
   div.outerHTML = '' // delete div completely
   return units1000 / 1000
